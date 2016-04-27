fb35c969225347407433bc90fb627f53d05a7580
Artiom Kenibasov <artiom.kenibasov@metrosystems.net>
2016-04-27 13:23:39 +0300

    Now solution compile3s (moskups for sqlite framework change)

--- Messaging/MessagingInterfaces/Repository/ContactRepository.cs
+++ Messaging/MessagingInterfaces/Repository/ContactRepository.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MessagingInterfaces.Model;
-using SQLite;
+//using SQLite;
 
 namespace MessagingInterfaces.Repository
 {
@@ -16,28 +16,29 @@ namespace MessagingInterfaces.Repository
         }
         public IEnumerable<Contact> GetContacts(string user)
         {
-            using (var db = new SQLiteConnection(@"c:\temp\contacts.db"))
-            {
-                var contacts = db.Table<Contact>().Where(x => x.Username == user).ToList();
-                return contacts;
-            }
+            //using (var db = new SQLiteConnection(@"c:\temp\contacts.db"))
+            //{
+            //    var contacts = db.Table<Contact>().Where(x => x.Username == user).ToList();
+            //    return contacts;
+            //}
+            return null;
         }
 
         public void SaveContacts(IEnumerable<Contact> contacts)
         {
-            using (var db = new SQLiteConnection(@"c:\temp\contacts.db"))
-            {
-                foreach (var contact in contacts)
-                    db.InsertOrReplace(contact);
-            }
+            //using (var db = new SQLiteConnection(@"c:\temp\contacts.db"))
+            //{
+            //    foreach (var contact in contacts)
+            //        db.InsertOrReplace(contact);
+            //}
         }
 
         private void InitializeDatabase()
         {
-            using (var db = new SQLiteConnection(@"c:\temp\contacts.db"))
-            {
-                db.CreateTable<Contact>();
-            }
+            //using (var db = new SQLiteConnection(@"c:\temp\contacts.db"))
+            //{
+            //    db.CreateTable<Contact>();
+            //}
         }
     }
 }
